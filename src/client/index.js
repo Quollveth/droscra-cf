8323ca02c1afb0300c2a4f835e0d2c5627177803
Quollveth <quollveth@gmail.com>
2025-05-15 16:22:45 -0300

    finally have something working

--- src/client/index.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("speaker").innerText = "Hello, World!";
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,18 +1,53 @@
-export interface Env {
-	MYBROWSER: Fetcher;
-	DB: D1Database;
-	DB_TOKEN: string;
-	SITE_URL: string;
+import { DurableObject } from 'cloudflare:workers';
+
+const DB_CREATE_QUERY = `
+	CREATE TABLE IF NOT EXISTS queries(
+		query TEXT PRIMARY KEY,
+		items INTEGER
+	);
+	`;
+
+export class DroscraObj extends DurableObject<Env> {
+	sql: SqlStorage;
+
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+
+		this.sql = ctx.storage.sql;
+		this.sql.exec(DB_CREATE_QUERY);
+	}
+
+	async speak(): Promise<string> {
+		let result = this.ctx.storage.sql.exec("SELECT 'Hello, World!' as greeting").one();
+		return result.greeting as string;
+	}
+
+	async save(data: string): Promise<boolean> {
+		this.sql.exec(`INSERT INTO queries (query, items) VALUES (?,0);`, [data]);
+		return true;
+	}
+
+	async load(): Promise<string> {
+		const cursor = this.sql.exec('SELECT * FROM queries;');
+
+		let ret = '';
+		for (let row of cursor) {
+			ret += row.query + ',';
+		}
+
+		return ret;
+	}
 }
 
 export default {
 	async fetch(request, env): Promise<Response> {
+		const pathname = new URL(request.url).pathname;
 		/*
 		if (request.method === 'OPTIONS') {
 			return new Response(null, {
 				status: 204,
 				headers: {
-					'Access-Control-Allow-Origin': env.SITE_URL,
+					'Access-Control-Allow-Origin': env.ORIGIN,
 					'Access-Control-Allow-Methods': 'POST, OPTIONS',
 					'Access-Control-Allow-Headers': 'Content-Type',
 				},
@@ -20,14 +55,41 @@ export default {
 		}
 		*/
 
-		const pathname = new URL(request.url);
+		const id: DurableObjectId = env.DROSCRA_OBJ.idFromName('object');
+		const stub = env.DROSCRA_OBJ.get(id);
 
-		if (pathname.pathname === '/hello') {
-			const squeal = "SELECT name FROM sqlite_schema WHERE type ='table'";
-			const result = await env.DB.prepare(squeal).all();
-			return Response.json(result);
+		try {
+			if (pathname === '/save') {
+				if (request.method !== 'POST') {
+					throw Error('invalid method');
+				}
+				const form = await request.formData();
+				const data = form.get('data');
+
+				await stub.save(data as string);
+				return Response.json(`data ${data} saved`);
+			}
+
+			if (pathname === '/load') {
+				if (request.method !== 'GET') {
+					throw Error('invalid method');
+				}
+				const data = await stub.load();
+				return Response.json(data);
+			}
+
+			if (pathname === '/speak') {
+				if (request.method !== 'GET') {
+					throw Error('invalid method');
+				}
+				const data = await stub.speak();
+				return Response.json(data);
+			}
+		} catch (e) {
+			console.log(e);
+			return Response.json(`exception: ${e}`);
 		}
 
-		return Response.json(pathname.pathname);
+		return Response.json(`Request sent to ${pathname}`);
 	},
 } satisfies ExportedHandler<Env>;
